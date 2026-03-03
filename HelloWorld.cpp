d64c81d61bcf560825b56b5f46dade9b346faac9
Alan <ve1.11@outlook.com>
2026-03-03 15:01:46 +0800

    init

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Arch Linux!" << std::endl;
+    
+        std::cout << "Running on Linux configuration." << std::endl;
+
+    return 0;
+}
